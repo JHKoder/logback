bf608057835abd6bcb6901b6ecec8c837dda7aff
oineh <jeonghun.kang.dev@gmail.com>
2023-01-30 21:40:45 +0900

    Initial commit

---
+++ src/main/java/github/oineh/logback/LogbackApplication.java
@@ -0,0 +1,13 @@
+package github.oineh.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogbackApplication.class, args);
+    }
+
+}
